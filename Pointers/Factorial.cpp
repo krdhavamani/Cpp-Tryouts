6f249faa26f769ad5b0e4445574705381c7a3d5e
Karthick Raja <krdhavamani@wpi.edu>
2020-05-26 09:47:29 -0700

    In-Progress, incomplete factorial program

--- Pointers/Factorial.cpp
+++ Pointers/Factorial.cpp
@@ -2,18 +2,19 @@
 
 #include <iostream>
 
-unsigned long long int factorial(int num, unsigned long long int *res)
+unsigned long long int factorial(unsigned long long int num, unsigned long long int *res)
 {
     if (num!=1)
-    {
         *res = num * factorial(num-1,res);
-    }
+    else
+        return *res;
 }
 
 int main()
 {
-    int number;
-    unsigned long long int *result = 1;
+    unsigned long long int number;
+    unsigned long long int initialValue = 1;
+    unsigned long long int *result = &initialValue;
     std::cout << "Enter a number to find its factorial:";
     std::cin >> number;
     *result = factorial(number, result);
